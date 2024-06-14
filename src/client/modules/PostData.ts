ec53734da4c9648deb53bfdbf30297dc9b5734d7
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-14 11:23:48 +0900

    new | PostData
    IPostData의 구현체 작성

---
+++ src/client/modules/PostData.ts
@@ -0,0 +1,15 @@
+import { CreateElement } from "./CreateElement";
+
+class PostData implements IPostData {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+
+  constructor(id : number, userId : number, title : string, body : string){
+    this.id = id;
+    this.userId = userId;
+    this.title = title;
+    this.body = body;
+  }
+}
