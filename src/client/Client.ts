8296e292a68797afcb9a8f632825f1fbd11b00c8
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-14 20:56:59 +0900

    modify | Client.ts
    모듈로 dummy.ts 호출

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -1,4 +1,6 @@
 import { ButtonEvent } from "./controllers/ButtonEvent";
+import { Hello } from "@shared/dummy";
 
+Hello();
 const $btn = document.getElementById("button")!;
 $btn.addEventListener("click", () => ButtonEvent());
